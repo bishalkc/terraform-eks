53c6c0bb827fa20ccb74a7c84b8a9a21442ae45c
Bishal KC <bishalkc@gmail.com>
2023-06-13 18:44:56 -0400

    fixing precommit

--- modules/kms/local.tf
+++ modules/kms/local.tf
@@ -9,7 +9,6 @@ locals {
 
   project        = var.project
   environment    = var.environment
-  tenant         = var.tenant
   aws_region     = data.aws_region.current.name
   account_id     = data.aws_caller_identity.current.account_id
   account_number = data.aws_caller_identity.current.account_id
